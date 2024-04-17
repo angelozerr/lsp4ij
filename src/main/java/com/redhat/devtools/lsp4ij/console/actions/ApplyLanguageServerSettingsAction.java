17cc43e16a5a8d94cf1b5da2066251a227ba1733
Mitja Leino <mituuuuz@hotmail.com>
2024-04-17 07:22:28 +0300

    Don't enable presentation if language server view is not valid

--- src/main/java/com/redhat/devtools/lsp4ij/console/actions/ApplyLanguageServerSettingsAction.java
+++ src/main/java/com/redhat/devtools/lsp4ij/console/actions/ApplyLanguageServerSettingsAction.java
@@ -80,7 +80,7 @@ public class ApplyLanguageServerSettingsAction extends AnAction {
             }
         }
 
-        e.getPresentation().setEnabled(modified);
+        e.getPresentation().setEnabled(modified && languageServerView.isValid());
     }
 
     @Override
